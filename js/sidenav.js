aabca23b938fe2cc2deef15004832dfb7d6dd8a6
davtor94 <37162062+davtor94@users.noreply.github.com>
2018-03-23 21:13:05 -0600

    formulario de agregar cita creado

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+    $("#addcita").click(function(){
+        $("#createCita").fadeToggle(3000);
+    });
+});
+
 function openNav() {
 
     document.getElementById("main").style.marginLeft = "15%";
@@ -10,14 +16,4 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
     document.getElementById("mySidenav").className = " sidenav  col-0";
-}
-$(document).ready(function(){
-    $("#addcita").click(function(){
-        $("#createCita").fadeout(3000);
-    });
-});
-
-function abrirCita() {
-  document.getElementById("createCita").fadeIn(3000);
-  document.getElementById("createCita").style.display = "flex";
 }
